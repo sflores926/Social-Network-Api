0c26a61d6e7f0f112f1b14c68c65ec8260959a5c
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-10-24 23:01:10 -0400

    created folders for config, models, assets,and index.js file, worked on models for user and reaction

---
+++ models/User.js
@@ -0,0 +1,59 @@
+const { Schema, model } = require('mongoose');
+
+const validateEmail = (email) => {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+};
+
+
+// Schema to create Post model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [validateEmail, "Please fill a valid email address"],
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                "Please fill a valid email address",
+            ],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+// Create a virtual property `friendCount` that gets the amount of friends per user
+userSchema
+    .virtual('friendCount')
+    // Getter
+    .get(function () {
+        return this.friends.length;
+    });
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
