0163c7b7f3324226439ba837d5be71e009bd5554
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-10-24 23:58:20 -0400

    created a controller folder worked on userController

---
+++ Routes/api/userRoutes.js
@@ -0,0 +1,16 @@
+//Used 18-NoSQL/01-Activities/25-Ins_CRUD-Subdoc/routes/api/userRoutes as reference
+
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser);
+
+module.exports = router;
