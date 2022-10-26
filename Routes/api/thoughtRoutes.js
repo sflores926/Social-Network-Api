bd0f6b1b6ac237755682adfc6c4d5523bb1e019a
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-10-25 22:39:38 -0400

    fixed error in userController.js

--- Routes/api/thoughtRoutes.js
+++ Routes/api/thoughtRoutes.js
@@ -13,7 +13,7 @@ const {
 // /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
-// /api/thougths/:thoughtId
+// /api/thoughts/:thoughtId
 router
   .route('/:thoughtId')
   .get(getSingleThought)
